d3bc3a8f48cdf9cde2ddf75ad9c2fba9f30f91b1
Nuttapat Koonarangsri <webmaster@hackinteach.com>
2018-02-08 23:49:59 +0700

    user can login, need to pass User to UserServlet

--- src/main/java/io/muic/cs/ooc/webapp/login/servlet/LoginServlet.java
+++ src/main/java/io/muic/cs/ooc/webapp/login/servlet/LoginServlet.java
@@ -1,6 +1,9 @@
 package io.muic.cs.ooc.webapp.login.servlet;
 
-import io.muic.cs.ooc.webapp.login.Routeable;
+import io.muic.cs.ooc.webapp.login.router.Routeable;
+import io.muic.cs.ooc.webapp.login.database.MySQL;
+import io.muic.cs.ooc.webapp.login.model.User;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -10,19 +13,45 @@ import java.io.IOException;
 
 public class LoginServlet extends HttpServlet implements Routeable {
 
+    private MySQL mySQL;
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/login.jsp");
+        RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/login.jsp");
         rd.include(request,response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        boolean passed = mySQL.authenticate(username, password);
+        request.removeAttribute("error");
 
+        if(username == null || password == null){
+            request.setAttribute("error",true);
+        }
+
+        if(passed){
+            System.out.println("Authenticate successful");
+            User user = mySQL.getUserbyUsername(username);
+            request.getSession().setAttribute("user", user);
+            request.getSession().removeAttribute("error");
+            response.sendRedirect("/user");
+        }
+        else{
+            request.getSession().setAttribute("error",true);
+            response.sendRedirect("/login");
+        }
     }
 
     @Override
     public String getMapping() {
         return "/login";
+    }
+
+    @Override
+    public void setMySQL(MySQL mySQL) {
+        this.mySQL = mySQL;
     }
 }
