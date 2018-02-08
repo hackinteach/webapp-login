2ecb5c616c8563e99e2daa6130cff2ce72ebbc94
Nuttapat Koonarangsri <webmaster@hackinteach.com>
2018-02-08 09:24:21 +0700

    Add JDBC utils

--- src/main/java/io/muic/cs/ooc/webapp/login/database/ConnectionUtils.java
+++ src/main/java/io/muic/cs/ooc/webapp/login/database/ConnectionUtils.java
@@ -1,4 +1,25 @@
 package io.muic.cs.ooc.webapp.login.database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionUtils {
+
+    public static Connection getConnection() throws SQLException, ClassNotFoundException {
+        String hostname = "localhost";
+        String port = "3307";
+        String dbName = "login";
+        String username = "ooc";
+        String password = "muic";
+        return getMySQLConnection(hostname,port,username,password,dbName);
+    }
+
+    public static Connection getMySQLConnection(String hostName,String port,String username, String password, String dbName) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        /* MYSQL url connection */
+        String connUrl = "jdbc:mysql://"+hostName+":"+port+"/"+dbName;
+        Connection conn = DriverManager.getConnection(connUrl,username,password);
+        return conn;
+    }
 }
--- src/main/java/io/muic/cs/ooc/webapp/login/database/MyUtils.java
+++ src/main/java/io/muic/cs/ooc/webapp/login/database/MyUtils.java
@@ -1,7 +1,60 @@
 package io.muic.cs.ooc.webapp.login.database;
 
-public class DBUtils {
+import io.muic.cs.ooc.webapp.login.user.User;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.sql.Connection;
+
+public class MyUtils {
     public static final String ATT_NAME_CONNECTION = "ATTRIBUTE_FOR_CONNECTION";
     private static final String ATT_NAME_USER_NAME = "ATTRIBUTE_FOR_STORE_USER_NAME_IN_COOKIE";
-    
+
+    public static void storeConnection(ServletRequest request, Connection connection){
+        request.setAttribute(ATT_NAME_CONNECTION,connection);
+    }
+
+    public static Connection getStoredConnection(ServletRequest request){
+        Connection connection = (Connection) request.getAttribute(ATT_NAME_CONNECTION);
+        return connection;
+    }
+
+    public static void storeLoginUser(HttpSession session, User loginUser){
+        session.setAttribute("loginUser",loginUser);
+    }
+
+    public static User getLoginUser(HttpSession session){
+        return (User) session.getAttribute("loginUser");
+    }
+
+    public static void storedUserCookie(HttpServletResponse response, User user){
+        System.out.println("Store user cookie");
+        Cookie cookieUser = new Cookie(ATT_NAME_USER_NAME,user.getUsername());
+        /* Save cookie for 10 mins */
+        cookieUser.setMaxAge(10*60);
+        response.addCookie(cookieUser);
+    }
+
+    public static String getUserCookie(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        if(cookies != null){
+            for(Cookie cookie : cookies){
+                if(ATT_NAME_USER_NAME.equals(cookie.getName())){
+                    return cookie.getValue();
+                }
+            }
+        }
+        return null;
+    }
+
+    public static void deleteUserCookie(HttpServletResponse response){
+        Cookie cookie = new Cookie(ATT_NAME_USER_NAME,null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+    }
+
+
 }
