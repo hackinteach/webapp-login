672699ec3a5033b3a8f464243788a3adb686abb2
Nuttapat Koonarangsri <webmaster@hackinteach.com>
2018-02-09 00:33:02 +0700

    Add logoutServlet

--- src/main/java/io/muic/cs/ooc/webapp/login/servlet/LogoutServlet.java
+++ src/main/java/io/muic/cs/ooc/webapp/login/servlet/LogoutServlet.java
@@ -1,8 +1,8 @@
 package io.muic.cs.ooc.webapp.login.servlet;
 
-import io.muic.cs.ooc.webapp.login.router.Routeable;
 import io.muic.cs.ooc.webapp.login.database.MySQL;
 import io.muic.cs.ooc.webapp.login.model.User;
+import io.muic.cs.ooc.webapp.login.router.Routeable;
 import org.apache.commons.lang.StringUtils;
 
 import javax.servlet.RequestDispatcher;
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class LoginServlet extends HttpServlet implements Routeable {
+public class LogoutServlet extends HttpServlet implements Routeable {
 
     private MySQL mySQL;
 
@@ -24,33 +24,13 @@ public class LoginServlet extends HttpServlet implements Routeable {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
-        boolean passed = mySQL.authenticate(username, password);
-
-        request.getSession().removeAttribute("invalidLogin");
-        request.getSession().removeAttribute("emptyField");
-
-        if(!StringUtils.isBlank(username) && !StringUtils.isBlank(password)){
-            if(passed){
-                System.out.println("Authenticate successful");
-                User user = mySQL.getUserbyUsername(username);
-                request.getSession().setAttribute("LOGIN_USER", user);
-                request.getSession().removeAttribute("error");
-                response.sendRedirect("/user");
-            }else{
-                request.getSession().setAttribute("invalidLogin",true);
-                response.sendRedirect("/login");
-            }
-        }else{
-            request.getSession().setAttribute("emptyField",true);
-            response.sendRedirect("/login");
-        }
+        request.getSession().invalidate();
+        response.sendRedirect("/login");
     }
 
     @Override
     public String getMapping() {
-        return "/login";
+        return "/logout";
     }
 
     @Override
