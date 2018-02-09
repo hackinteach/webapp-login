0955d9fb5521559e0507c99d2b0bc68657a56b83
Nuttapat Koonarangsri <webmaster@hackinteach.com>
2018-02-10 00:57:35 +0700

    Finish simple register and login

--- src/main/java/io/muic/cs/ooc/webapp/login/services/RegisterService.java
+++ src/main/java/io/muic/cs/ooc/webapp/login/services/RegisterService.java
@@ -1,4 +1,47 @@
 package io.muic.cs.ooc.webapp.login.services;
 
-public class RegisterService {
+import io.muic.cs.ooc.webapp.login.database.MySQL;
+import io.muic.cs.ooc.webapp.login.model.User;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class RegisterService extends Service{
+
+    private HttpServletResponse response;
+    private HttpServletRequest request;
+
+    public RegisterService(HttpServletRequest request, HttpServletResponse response){
+        this.request = request;
+        this.response = response;
+    }
+
+    public boolean register(String username, String password){
+        return mySQL.createUser(username,password);
+    }
+
+    public boolean dupUser(String username){
+        return mySQL.isUserExists(username);
+    }
+
+    public User getUser(String username){
+        return mySQL.getUserbyUsername(username);
+    }
+
+    @Override
+    public void error(String msg, String path) {
+        try {
+            System.out.println(msg);
+            request.setAttribute("error",msg);
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher(path);
+            requestDispatcher.include(request,response);
+        } catch (ServletException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
