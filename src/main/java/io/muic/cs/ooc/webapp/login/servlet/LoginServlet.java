591014af10626f82fa2a4bd65ac4be1c154ab832
Nuttapat Koonarangsri <webmaster@hackinteach.com>
2018-02-11 19:22:05 +0700

    Change mySQL to static and verify cookie if already logged in

--- src/main/java/io/muic/cs/ooc/webapp/login/servlet/LoginServlet.java
+++ src/main/java/io/muic/cs/ooc/webapp/login/servlet/LoginServlet.java
@@ -4,18 +4,18 @@ import io.muic.cs.ooc.webapp.login.router.Routeable;
 import io.muic.cs.ooc.webapp.login.database.MySQL;
 import io.muic.cs.ooc.webapp.login.model.User;
 import io.muic.cs.ooc.webapp.login.services.LoginService;
+import io.muic.cs.ooc.webapp.login.utils.CookieUtil;
 import org.apache.commons.lang.StringUtils;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class LoginServlet extends HttpServlet implements Routeable {
-
-    private MySQL mySQL;
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -29,30 +29,29 @@ public class LoginServlet extends HttpServlet implements Routeable {
         String password = request.getParameter("password");
         LoginService lis = new LoginService(request,response);
 
+        if(CookieUtil.verifyCookie(request,response)){
+            response.sendRedirect("/user");
+        }
+
         if(!StringUtils.isBlank(username) && !StringUtils.isBlank(password)){
             boolean passed = lis.login(username,password);
             if(passed){
                 System.out.println("Authenticate successful");
-                User user = mySQL.getUserbyUsername(username);
+                User user = MySQL.getUserbyUsername(username);
                 request.getSession().setAttribute("user", user);
                 response.sendRedirect("/user");
             }else{
                 String error = "Invalid Login";
-                lis.error(error, "WEB-INF/login.jsp");
+                lis.error(request,response,error, "WEB-INF/login.jsp");
             }
         }else{
             String error = "Username and Password cannot be blank";
-            lis.error(error,"WEB-INF/login.jsp");
+            lis.error(request,response,error,"WEB-INF/login.jsp");
         }
     }
 
     @Override
     public String getMapping() {
         return "/login";
-    }
-
-    @Override
-    public void setMySQL(MySQL mySQL) {
-        this.mySQL = mySQL;
     }
 }
