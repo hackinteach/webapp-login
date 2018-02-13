41d8db2c755b9050e4fb0b9e5c4b39dc8d8ebbf5
Nuttapat Koonarangsri <webmaster@hackinteach.com>
2018-02-13 15:49:56 +0700

    Add RemoveUserServlet

--- src/main/java/io/muic/cs/ooc/webapp/login/servlet/RemoveUserServlet.java
+++ src/main/java/io/muic/cs/ooc/webapp/login/servlet/RemoveUserServlet.java
@@ -1,12 +1,32 @@
 package io.muic.cs.ooc.webapp.login.servlet;
 
+import io.muic.cs.ooc.webapp.login.database.MySQL;
+import io.muic.cs.ooc.webapp.login.model.User;
 import io.muic.cs.ooc.webapp.login.router.Routeable;
+import io.muic.cs.ooc.webapp.login.utils.CookieUtil;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-public class DeleteServlet extends HttpServlet implements Routeable {
+public class RemoveUserServlet extends HttpServlet implements Routeable {
     @Override
     public String getMapping() {
-        return "/";
+        return "/remove";
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("In remove");
+        if(!CookieUtil.verifyCookie(req,resp)){
+//            resp.sendRedirect("/login");
+            return;
+        }
+        String username = req.getParameter("removeUser");
+        System.out.println("Removing " +username);
+        MySQL.removeUserbyUsername(username);
+        resp.sendRedirect("/user");
     }
 }
