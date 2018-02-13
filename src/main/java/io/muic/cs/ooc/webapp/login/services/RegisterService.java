38db72fd70a55d0ebd041989b06183607fac487c
Nuttapat Koonarangsri <webmaster@hackinteach.com>
2018-02-13 11:38:53 +0700

    get the rest of fields

--- src/main/java/io/muic/cs/ooc/webapp/login/services/RegisterService.java
+++ src/main/java/io/muic/cs/ooc/webapp/login/services/RegisterService.java
@@ -3,11 +3,8 @@ package io.muic.cs.ooc.webapp.login.services;
 import io.muic.cs.ooc.webapp.login.database.MySQL;
 import io.muic.cs.ooc.webapp.login.model.User;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 public class RegisterService extends Service{
 
@@ -19,15 +16,23 @@ public class RegisterService extends Service{
         this.response = response;
     }
 
-    public boolean register(String username, String password){
-        return mySQL.createUser(username,password);
+    public boolean register(String username,
+                            String password,
+                            String firstname,
+                            String lastname,
+                            String email){
+        return MySQL.createUser(username,password,firstname,lastname,email);
     }
 
     public boolean dupUser(String username){
-        return mySQL.isUserExists(username);
+        return MySQL.isUserExists(username);
     }
 
     public User getUser(String username){
-        return mySQL.getUserbyUsername(username);
+        return MySQL.getUserbyUsername(username);
+    }
+
+    public boolean dupEmail(String email) {
+        return MySQL.isEmailExists(email);
     }
 }
