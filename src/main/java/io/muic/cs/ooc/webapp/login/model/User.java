4d8dfc9fe870fd7b5ca6e78941af412082b22999
Nuttapat Koonarangsri <webmaster@hackinteach.com>
2018-02-13 11:46:38 +0700

    Add firstname lastname and email fields

--- src/main/java/io/muic/cs/ooc/webapp/login/model/User.java
+++ src/main/java/io/muic/cs/ooc/webapp/login/model/User.java
@@ -20,6 +20,39 @@ public class User extends AbstractEntity implements Serializable {
     @Column(nullable = false)
     private String password;
 
+    @Column(nullable = false)
+    private String firstname;
+
+    @Column(nullable = false)
+    private String lastname;
+
+    @Column(nullable = false)
+    private String email;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getUsername() {
         return username;
     }
