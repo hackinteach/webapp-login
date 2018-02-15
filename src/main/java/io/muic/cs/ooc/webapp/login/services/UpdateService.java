177ce9f8980b68a4bec702aeaadd36f72e512577
Nuttapat Koonarangsri <webmaster@hackinteach.com>
2018-02-16 01:20:08 +0700

    use id for identification instead of username

--- src/main/java/io/muic/cs/ooc/webapp/login/services/UpdateService.java
+++ src/main/java/io/muic/cs/ooc/webapp/login/services/UpdateService.java
@@ -8,12 +8,15 @@ import java.util.Map;
 public class UpdateService extends Service{
 
     public void updateUser(String username,Map<String,String> newProfile) {
-        System.out.println("update "+username);
+
+        String id = MySQL.getUserbyUsername(username).getid().toString();
+
         for(String key : newProfile.keySet()){
             String val = newProfile.get(key);
+
             if(!StringUtils.isBlank(val))
             {
-                MySQL.updateInfo(username,key,newProfile.get(key));
+                MySQL.updateInfo(id,key,newProfile.get(key));
             }
         }
     }
